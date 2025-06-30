1caa3c9e64ee04d164a79e26179e4cfdc1e90d06
PokeSidV <pokesidv@gmail.com>
2025-06-30 18:37:39 +0200

    created first town and lab

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1157,3 +1157,6 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/dexnav.inc"
+	.include "data/maps/ChitinCragDale/scripts.inc"
+
+	.include "data/maps/ChitinCragDale_Lab/scripts.inc"
