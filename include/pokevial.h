32fa75a78515bcd9bcbfd4fa81f114ecbc9174dc
pokesidv <216572922+pokesidv@users.noreply.github.com>
2025-07-09 02:58:54 +0200

    qol menu pokevial item

--- include/pokevial.h
+++ include/pokevial.h
@@ -30,6 +30,6 @@ enum pokevialPercentConstant {
     POKEVIAL_ICON_PERCENT_100,
 };
 
-#define POKEVIAL_SKIP_CUTSCENE  FALSE // When this FALSE is changed to TRUE, when using the Pokevial, the item will cut to the party screen and use the HealPlayerParty special instead.
+#define POKEVIAL_SKIP_CUTSCENE  TRUE // When this FALSE is changed to TRUE, when using the Pokevial, the item will cut to the party screen and use the HealPlayerParty special instead.
 
 #endif // GUARD_POKEVIAL_H
