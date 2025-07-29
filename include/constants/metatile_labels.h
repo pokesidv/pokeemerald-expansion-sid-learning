a968db112c2d26ad49908e186c4cb1472151a3b2
pokesidv <216572922+pokesidv@users.noreply.github.com>
2025-07-29 10:22:49 +0200

    testing map for rock climb in fallarbor

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -179,6 +179,7 @@
 #define METATILE_Fallarbor_BrownCaveIndent           0x208
 #define METATILE_Fallarbor_BrownCaveOpen             0x210
 #define METATILE_Fallarbor_BrownRockWall             0x265
+#define METATILE_Fallarbor_ClimbableRockWallLeft     0x369
 #define METATILE_Fallarbor_Door_BattleTent           0x36C
 #define METATILE_Fallarbor_Door_DarkRoof             0x2F7
 #define METATILE_Fallarbor_Door_LightRoof            0x2A5
