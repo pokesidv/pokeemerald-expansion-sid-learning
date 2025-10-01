48fd306351b01859d1be7c5d5603529a6c09a147
pokesidv <216572922+pokesidv@users.noreply.github.com>
2025-10-01 12:37:17 +0200

    fix: removed wall clock settings

--- src/debug.c
+++ src/debug.c
@@ -1540,8 +1540,8 @@ static void DebugAction_Player_Id(u8 taskId)
 
 static void DebugAction_Util_CheatStart(u8 taskId)
 {
-    if (!FlagGet(FLAG_SYS_CLOCK_SET))
-        RtcInitLocalTimeOffset(0, 0);
+    // if (!FlagGet(FLAG_SYS_CLOCK_SET))
+    //     RtcInitLocalTimeOffset(0, 0);
 
     InitTimeBasedEvents();
     Debug_DestroyMenu_Full_Script(taskId, Debug_CheatStart);
